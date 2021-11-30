6824badb31319d6cf4a33ca7b281faefa863e910
Marek Dudek <marek.dudek@gmail.com>
2021-11-30 21:16:40 +0100

    fresh project

---
+++ src/main/java/md/springbootgraphitegrafana/SpringBootGraphiteGrafanaApplication.java
@@ -0,0 +1,15 @@
+package md.springbootgraphitegrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphiteGrafanaApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootGraphiteGrafanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/md/springbootgraphitegrafana/SpringBootGraphiteGrafanaApplicationTests.java
@@ -0,0 +1,15 @@
+package md.springbootgraphitegrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphiteGrafanaApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
